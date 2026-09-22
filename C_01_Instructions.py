12e15ba2cf2d4b687bccd8c7c8e3e0eeea682d38
HungryBoneBreaker <medillaj50277@masseyhigh.school.nz>
2026-09-23 11:56:47 +1200

    Update C_01_Instructions.py

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -5,18 +5,20 @@ def statement_generator(statement, decoration):
 
 # Display function here
 def instructions():
-    statement_generator("Instruction","-")
+    statement_generator("Instructions","-")
 
     print('''
-Instructions goes here
-- Instruction 1
-- Instruction 2
-- whatever anything left
+To use the progress, you simply need to type in a number that is equal or more than 1 to less or equal 200
+the program will also show the numbers' factor based on your chosen integer
+
+The program will also say if your chosen number is a prime number, or a perfect square :)
+- Is a prime number
+- Or if it's a perfect square
 ''')
 
-#Main Routine goes here
+# The Main Routine goes here
 
-# Display instruction if client commanded so
+# Display instruction if the user commanded so
 want_instructions = input("Press <Enter> to see instruction or type anything then press <Enter> to Run with no Instructions ")
 
 
@@ -24,4 +26,4 @@ if want_instructions == "":
     instructions()
 
 print()
-print(" Alrighty!")
+print(" Alrighty, let's proceed")
