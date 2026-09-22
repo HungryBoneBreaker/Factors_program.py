8da63bfe823e7a5ecb38f77d59dcb5a9e9e4a059
HungryBoneBreaker <medillaj50277@masseyhigh.school.nz>
2026-09-23 11:58:12 +1200

    Update C_02_Integer_Checker.py
    Updated code

--- C_02_Integer_Checker.py
+++ C_02_Integer_Checker.py
@@ -1,30 +1,31 @@
-# Ask the user for a number and make sure it has no decimals, is more or equal to 1 and less or equal to 200
-def int_check(question, low, high):
-    low_error = f"Please enter a number that is more or equal to {low}\n"
-    high_error = f"Please enter a number that is less or equal to {high}\n"
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def num_check(question):
+
+    error = f"Please enter a number that is between 1 and 200 inclusive\n"
     while True:
 
+        response = input(question).lower()
+        if response == "xxx":
+            return response
+
         try:
-            # ask the human for a number
-            response = int(input(question))
+            # Ask the user for a number
+            response = int(response)
 
-            # check that the number is more than or equal to 1 and is less or equal to 200
-            if response >= low:
+            # Check that the number is more than zero and less than 201
+            if 1 <= response <= 200:
                 return response
             else:
-                print(low_error)
-                print("and make sure the integer has no decimals!")
-
-            if response <= high:
-                return response
-            else:
-                print(high_error)
-                print("and make sure the integer has no decimals!")
-
+                print(error)
         except ValueError:
-            print("Please make sure the integer is valid"
-                  "e.g no decimals, equal or more than 1 and equal or less than 200")
+            print(error)
+
 
 # Main routine goes here
-integer = int_check("Integer:", 1, 200)
-print(f"Your integer is {integer}\n")
+while True:
+    to_factor = num_check("To factor: ")
+    print("You chose to factor", to_factor)
+
+    if to_factor == "xxx":
+        break
